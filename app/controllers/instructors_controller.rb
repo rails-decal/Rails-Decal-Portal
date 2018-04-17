8279034b5caadd243fc55c389aa2a7afbbb18834
Emily Zhong <emilyzhongsihua@gmail.com>
2018-04-17 01:53:32 -0700

    unecessary controller

--- app/controllers/instructors_controller.rb
+++
@@ -1,2 +0,0 @@
-class InstructorsController < ApplicationController
-end
--- test/controllers/instructors_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InstructorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
