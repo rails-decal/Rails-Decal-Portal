ecbf2b6ad85929307bd2ca26bcd550e60e0f1d7b
Emily Zhong <emilyzhongsihua@gmail.com>
2018-04-20 02:08:59 -0700

    lots of front end (mainly curric and assignments view)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,30 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+const highlightBlue = "#EDF6FD";
+const blue = "#6E9AFF";
+$(document).on('turbolinks:load', function() {
+	let navBarItem = "#" + $(".title").text().toLowerCase();
+	$(navBarItem).css("background", highlightBlue);
+	$(navBarItem).css("color", blue);
+
+	$(document).ready(function() {
+		$(".resource-edit-button.edit").click(function() {
+			let modal = $(this).parent().parent().parent().next();
+			modal.fadeIn();
+			modal.css("top", "50%");
+			$(".overlay").fadeIn();
+		})
+
+		$(".overlay, .close").click(function() {
+			$(".modal-form").css("top", "150%");
+			setTimeout(function() {
+				$(".modal-form, .overlay").fadeOut();
+			}, 100)
+		})
+	})
+})
