17fc8f95d6634ecafd88ebb7f96958cb8ce320d3
Franco <christopher.franco.m@gmail.com>
2018-04-24 12:26:43 -0700

    cleaned up changes from master

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,22 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'attendances/index'
-
-  get 'attendances/create'
-
-  get 'attendances/new'
-
-  get 'attendances/edit'
-
-  get 'attendances/show'
-
-  get 'attendances/update'
-
-  get 'attendances/destroy'
-
-=======
   devise_for :admins
   devise_for :students
->>>>>>> master
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "semesters#index"
-
   get 'sign_in', to: 'login#choose'
   get 'sign_out', to: 'login#sign_out'
 
   resources :semesters do
     resources :weeks, only: [:create, :new, :edit, :update, :destroy]
-    resources :assignments, only: [:index, :destroy, :create]
+    resources :assignments, only: [:index, :destroy]
   end
 
   resources :weeks do
     resources :assignments, only: [:create, :new, :edit, :update]
     resources :resources
   end
-
 
   resources :assignments do
     resources :submissions
