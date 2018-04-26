6bae10c53edb4f7ac2b1ad1586e206a78da36270
aivantg <aivant.developer@gmail.com>
2018-04-26 02:54:28 -0700

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180425181838) do
 
   create_table "admins", force: :cascade do |t|
     t.boolean "active", default: true
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "name"
     t.string "office_hours"
     t.string "picture"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20180425181838) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "picture"
     t.integer "semester_id"
     t.datetime "created_at", null: false
