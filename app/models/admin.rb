07a875fe58aae2e5935f55b8346d3b59d779a366
aivantg <aivant.developer@gmail.com>
2018-09-06 14:05:48 -0700

    Fixed Admin edit profile feature

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -15,7 +15,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_scope :admin do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_admin_registration" # custom path to sign_up/registration
+  end
+
+  devise_for :admins, :skip => [:registrations]
+    as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
+
   devise_for :students
 
   mount RailsAdmin::Engine => '/config', as: 'rails_admin'
@@ -29,5 +39,7 @@ Rails.application.routes.draw do
   end
 
   resources :submissions, only: [:update]
+
+
 
 end
