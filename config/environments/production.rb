7d01024c56f5e7e65bd6a177a78e370ebb7b858f
aivantg <aivant.developer@gmail.com>
2018-09-11 17:31:59 -0700

    added ability to change office hours for admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Allows requests to come from any origin. Still need CSRF token however so we're still secure
   config.action_controller.forgery_protection_origin_check = false
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
